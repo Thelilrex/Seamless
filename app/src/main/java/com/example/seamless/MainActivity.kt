353c08bcb7a8b3f61eaa4171f07430ade718de7f
fufukoku <fufukokunof@gmail.com>
2024-03-06 11:10:28 +0900

    Improve UI buttons
    1. Created Personal/ Business functions
    2. Created Jump functions for Personal/ Business functions
    3. Made cards  to the centre

--- app/src/main/java/com/example/seamless/MainActivity.kt
+++ app/src/main/java/com/example/seamless/MainActivity.kt
@@ -1,24 +1,32 @@
 package com.example.seamless
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -45,28 +53,35 @@ class MainActivity : ComponentActivity() {
     }
 }
 //UI
-@Composable
-fun LogInPart() {
-    Row{
-        Button(onClick = {
-            // enter the settings
-        })
-        {
-            Text("Log in")
-        }
-        Button(onClick = {
-            // enter the settings
-        })
-        {
-            Text("Sign up")
-        }
-    }
-}
+//@Composable
+//fun LogInPart() {
+//    Row{
+//        Button(onClick = {
+//            // enter the settings
+//        })
+//        {
+//            Text("Log in")
+//        }
+//        Button(onClick = {
+//            // enter the settings
+//        })
+//        {
+//            Text("Sign up")
+//        }
+//    }
+//}
 @Composable
 fun SettingPart()
 {
     Row{
-        Button(onClick = {
+        OutlinedButton(
+            //Colors = Color.Blue,
+            modifier = Modifier
+                .height(60.dp)
+                .padding(10.dp)
+                .size(100.dp)
+                ,
+            onClick = {
             // enter the settings
         })
         {
@@ -78,10 +93,17 @@ fun SettingPart()
 @Composable
 fun FunctionList(functionList: List<Function>, modifier:Modifier = Modifier)
 {
-    LazyColumn {
+    LazyColumn (
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
         items(functionList){
             function: Function -> FunctionCard(function = function,
-            modifier = Modifier.padding(40.dp))
+            modifier = Modifier.padding(20.dp),
+
+            )
 
         }
     }
@@ -99,25 +121,74 @@ fun MainPart()
 @Composable
 fun FunctionCard(function: Function, modifier: Modifier = Modifier)
 {
-    Card(modifier = modifier)
+    //Space()
+    Card(
+        modifier = modifier.padding(10.dp,30.dp)
+    )
     {
-        Column {
+
+        Column (modifier = modifier)
+        {
             Image(
                 painter = painterResource(id = function.imageResourceId),
                 contentDescription = stringResource(id = function.functionResourceId),
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(90.dp),
+                    .fillMaxWidth().height(130.dp),
                 contentScale = ContentScale.Crop
             )
-            Text(
-                text = stringResource(id = function.functionResourceId),
-                modifier = Modifier
-                .height(40.dp),
-                style = MaterialTheme.typography.headlineSmall
-            )
+            Button(
+                modifier = Modifier.fillMaxSize(),
+                onClick = {
+                    // go to personal/business functions
+                    DecideFunction()
+                }) {
+                Text(
+                    text = stringResource(id = function.functionResourceId),
+                    modifier = Modifier
+                        .height(40.dp),
+                    style = MaterialTheme.typography.headlineSmall
+                )
+            }
+
         }
     }
+}
+
+fun DecideFunction (// get functionResourceId
+)
+{
+    if(
+        true
+        // functionResourceId == Personal function
+    )
+    {
+        // jump to PersonalFunctionCard
+    }
+    if(
+        true
+        // functionResourceId == Business function
+    )
+    {
+        // jump to PersonalFunctionCard
+    }
+    else
+    {
+        // Error
+    }
+}
+fun PersonalFunctionJumper(function: Function, modifier: Modifier = Modifier)
+{
+    // go to PersonalFunctionCard
+}
+
+fun BusinessFunctionCardJumper(function: Function, modifier: Modifier = Modifier)
+{
+    // go to BusinessFunctionCard
+}
+
+fun SettingsJumper()
+{
+    // go to SettingFunctionCard
 }
 
 @Composable
--- app/src/main/java/data/DataSource.kt
+++ app/src/main/java/data/DataSource.kt
@@ -8,14 +8,14 @@ class DataSource {
         return listOf<Function>(
             Function(R.string.function1, R.drawable.testimage),
             Function(R.string.function2, R.drawable.testimage),
-            Function(R.string.function3, R.drawable.testimage),
-            Function(R.string.function4, R.drawable.testimage),
-            Function(R.string.function5, R.drawable.testimage),
-            Function(R.string.function6, R.drawable.testimage),
-            Function(R.string.function7, R.drawable.testimage),
-            Function(R.string.function8, R.drawable.testimage),
-            Function(R.string.function9, R.drawable.testimage),
-            Function(R.string.function10, R.drawable.testimage)
+ //           Function(R.string.function3, R.drawable.testimage),
+//            Function(R.string.function4, R.drawable.testimage),
+//            Function(R.string.function5, R.drawable.testimage),
+//            Function(R.string.function6, R.drawable.testimage),
+//            Function(R.string.function7, R.drawable.testimage),
+//            Function(R.string.function8, R.drawable.testimage),
+//            Function(R.string.function9, R.drawable.testimage),
+//            Function(R.string.function10, R.drawable.testimage)
         )
     }
 }
