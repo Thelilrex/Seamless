444793bcfd287d1f4e20f064b83e7324a04f6e11
Isamu Sherman <51054422+uncannierlink@users.noreply.github.com>
2024-03-08 14:42:57 +0900

    Changed Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,23 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    val nav_version = "2.7.7"
+
+    // Java language implementation
+    implementation("androidx.navigation:navigation-fragment:$nav_version")
+    implementation("androidx.navigation:navigation-ui:$nav_version")
+
+    // Kotlin
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    // Feature module Support
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
+
+    // Testing Navigation
+    androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
+
+    // Jetpack Compose Integration
+    implementation("androidx.navigation:navigation-compose:$nav_version")
 }
--- app/src/main/java/com/example/seamless/SeamlessScreen.kt
+++ app/src/main/java/com/example/seamless/SeamlessScreen.kt
@@ -1,8 +1,19 @@
 package com.example.seamless
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
 enum class SeamlessScreen {
     Start,
     Personal,
     Business
 }
 
+@Composable
+fun SeamlessApp(
+    navController: NavController = rememberNavController()
+){
+
+}
