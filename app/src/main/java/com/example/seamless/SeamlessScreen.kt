dcabbcb42a4dca8c5a11c17abe6a3b5c030b3ab4
Isamu Sherman <51054422+uncannierlink@users.noreply.github.com>
2024-03-08 15:09:20 +0900

    Created Navhost and Composables

--- app/src/main/java/com/example/seamless/SeamlessScreen.kt
+++ app/src/main/java/com/example/seamless/SeamlessScreen.kt
@@ -2,7 +2,10 @@ package com.example.seamless
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.navigation.NavController
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 enum class SeamlessScreen {
@@ -13,7 +16,20 @@ enum class SeamlessScreen {
 
 @Composable
 fun SeamlessApp(
-    navController: NavController = rememberNavController()
 ){
+    val navController = rememberNavController()
 
+    NavHost(navController = navController, startDestination = SeamlessScreen.Start.name) {
+        composable(route = SeamlessScreen.Start.name) {
+
+        }
+
+        composable(route = SeamlessScreen.Personal.name) {
+
+        }
+
+        composable(route = SeamlessScreen.Business.name) {
+
+        }
+    }
 }
