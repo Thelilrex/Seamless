115af7c34b3a852d1712c0def1ac582fd7705a57
Isamu Sherman <51054422+uncannierlink@users.noreply.github.com>
2024-03-11 12:53:52 +0900

    Added onbuttonclick funcs to navigation compos

--- app/src/main/java/com/example/seamless/SeamlessScreen.kt
+++ app/src/main/java/com/example/seamless/SeamlessScreen.kt
@@ -7,6 +7,8 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.seamless.ui.screens.BusinessScreen
+import com.example.seamless.ui.screens.PersonalScreen
 import com.example.seamless.ui.screens.StartScreen
 
 enum class SeamlessScreen {
@@ -23,16 +25,21 @@ fun SeamlessApp(
     NavHost(navController = navController, startDestination = SeamlessScreen.Start.name) {
         composable(route = SeamlessScreen.Start.name) {
             StartScreen(
-                
+                onPersonalButtonClicked = {
+                    navController.navigate(SeamlessScreen.Personal.name)
+                },
+                onBusinessButtonClicked = {
+                    navController.navigate(SeamlessScreen.Business.name)
+                },
                 modifier = Modifier)
         }
 
         composable(route = SeamlessScreen.Personal.name) {
-
+            PersonalScreen(modifier = Modifier)
         }
 
         composable(route = SeamlessScreen.Business.name) {
-
+            BusinessScreen(modifier = Modifier)
         }
     }
 }
