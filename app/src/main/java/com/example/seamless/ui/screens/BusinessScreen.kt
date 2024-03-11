8b771217f7ff2207abad3781c6bcf0f2c7b7b506
Isamu Sherman <51054422+uncannierlink@users.noreply.github.com>
2024-03-11 12:38:19 +0900

    Added buttons to BusinessScreen composable

--- app/src/main/java/com/example/seamless/ui/screens/BusinessScreen.kt
+++ app/src/main/java/com/example/seamless/ui/screens/BusinessScreen.kt
@@ -1,2 +1,36 @@
 package com.example.seamless.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun BusinessScreen(
+    modifier: Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Button(
+            onClick = {},
+            modifier = modifier
+        ){
+            Text(text = "Do Something")
+        }
+        Button(
+            onClick = {},
+            modifier = modifier
+        ){
+            Text(text = "Home")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun BusinessScreenPreview() {
+    BusinessScreen(modifier = Modifier)
+}
