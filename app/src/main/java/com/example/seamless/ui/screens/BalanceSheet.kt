bd8324b20dda111f84c278b43d73b4ef3224c272
fufukoku <fufukokunof@gmail.com>
2024-04-05 11:06:16 +0900

    Add Revenues and Expenses to Income Statement Tracks

--- app/src/main/java/com/example/seamless/ui/screens/BalanceSheet.kt
+++ app/src/main/java/com/example/seamless/ui/screens/BalanceSheet.kt
@@ -1,2 +1,24 @@
 package com.example.seamless.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.seamless.R
+
