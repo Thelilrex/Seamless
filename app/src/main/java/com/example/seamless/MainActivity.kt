e55c4037586638937852a99183a440153829cdf8
fufukoku <fufukokunof@gmail.com>
2024-04-08 11:48:30 +0900

    debug

--- app/src/main/java/com/example/seamless/MainActivity.kt
+++ app/src/main/java/com/example/seamless/MainActivity.kt
@@ -32,7 +32,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.seamless.ui.screens.PersonalIncomeScreen
+import com.example.seamless.ui.screens.PersonalIncomesScreen
 import com.example.seamless.ui.screens.PersonalScreen
 import com.example.seamless.ui.screens.PersonalSpendsScreen
 import com.example.seamless.ui.screens.StartScreen
