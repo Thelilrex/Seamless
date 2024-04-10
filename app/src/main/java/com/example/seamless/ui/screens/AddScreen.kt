faf2f60392b00142893591e5ec6b04d6b34e700b
fufukoku <fufukokunof@gmail.com>
2024-04-10 13:13:09 +0900

    Done with add screen

--- app/src/main/java/com/example/seamless/ui/screens/AddScreen.kt
+++ app/src/main/java/com/example/seamless/ui/screens/AddScreen.kt
@@ -1,18 +1,24 @@
 package com.example.seamless.ui.screens
 
+import android.widget.Space
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -22,19 +28,43 @@ fun AddCatagories() {
     val name = remember { mutableStateOf("") }
     val description = remember { mutableStateOf("") }
     val amount = remember { mutableStateOf("") }
+    val defaultPadding = 8.dp
 
-    Column(modifier = Modifier) {
+    Column(modifier = Modifier.padding(defaultPadding)) {
         Row(
             modifier = Modifier
                 .horizontalScroll(rememberScrollState())
                 .padding(16.dp).height(50.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
         ) {
-
+            Button(modifier = Modifier.height(50.dp),
+                onClick = {  }) {
+                Text("Button1")
+            }
+            Button(modifier = Modifier.height(50.dp),
+                onClick = { /* onclick */ }) {
+                Text("Button2")
+            }
+            Button(modifier = Modifier.height(50.dp),
+                onClick = { /* onclick */ }) {
+                Text("Button3")
+            }
+            Button(modifier = Modifier.height(50.dp),
+                onClick = { /* onclick */ }) {
+                Text("Button4")
+            }
+            Button(modifier = Modifier.height(50.dp),
+                onClick = { /* onclick */ }) {
+                Text("Button5")
+            }
+            Button(modifier = Modifier.height(50.dp),
+                onClick = { /* onclick */ }) {
+                Text("Add+")
+            }
         }
+
         Row(
             modifier = Modifier
-                .horizontalScroll(rememberScrollState())
                 .padding(16.dp).height(50.dp),
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
@@ -42,43 +72,61 @@ fun AddCatagories() {
                 value = name.value,
                 onValueChange = { name.value = it },
                 label = { Text("Name") },
-                modifier = Modifier.weight(1f)
+                modifier = Modifier.fillMaxWidth()
             )
         }
         Row(
             modifier = Modifier
-                .horizontalScroll(rememberScrollState())
                 .padding(16.dp).height(50.dp),
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             OutlinedTextField(
-                        value = description.value,
-                        onValueChange = { description.value = it },
-                        label = { Text("Description") },
-                        modifier = Modifier.weight(1f)
-                    )
+                value = description.value,
+                onValueChange = { description.value = it },
+                label = { Text("Description") },
+                modifier = Modifier.fillMaxWidth()
+            )
         }
         Row(
             modifier = Modifier
-                .horizontalScroll(rememberScrollState())
                 .padding(16.dp).height(50.dp),
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             OutlinedTextField(
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                 value = amount.value,
                 onValueChange = { amount.value = it },
                 label = { Text("Amount") },
-                modifier = Modifier.weight(1f)
+                modifier = Modifier.fillMaxWidth()
             )
         }
-        //Buttons
+
         Row(
             modifier = Modifier
-                .horizontalScroll(rememberScrollState())
-                .padding(16.dp).height(50.dp),
+                .padding(16.dp)
+                .height(50.dp),
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
+            Button(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight(),
+                onClick = {
 
+                }
+            ) {
+                Text("Cancel")
+            }
+            Button(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxHeight(),
+                onClick = {
+
+                }
+            ) {
+                Text("Confirm")
+            }
         }
     }
 }
